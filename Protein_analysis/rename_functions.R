f176628345ab6180a4e3ff781a490d68f6e25548
Amanda Figueroa <38810464+Cleanvalidation@users.noreply.github.com>
2022-05-26 16:02:09 -0400

    Update rename_functions.R
    Added additional ifelse statements to detect column names in different versions of PD

--- Protein_analysis/rename_functions.R
+++ Protein_analysis/rename_functions.R
@@ -8,48 +8,67 @@ read_xl<-function(x,solvent) {
   df2<-df2 %>% dplyr::select(-tidyr::starts_with("Found"))
   
   if(any(names(df2)=="Percolator.PEP.by.Search.Engine.Sequest.HT")){
-    df2<-df2 %>% dplyr::rename("Percolator_PEP"="Percolator.PEP.by.Search.Engine.Sequest.HT")
-  }else if(any(names(df2)=="Percolator.PEP")){
-    df2<-df2 %>% dplyr::rename("Percolator_PEP"="Percolator.PEP")
+    df2<-df2 %>% dplyr::rename("Percolator_PEP"=names(df2)[stringr::str_detect(names(df2),"PEP")])
+  }else if(any(stringr::str_detect(names(df2),"PEP"))){
+    df2<-df2 %>% dplyr::rename("Percolator_PEP"=names(df2)[stringr::str_detect(names(df2),"PEP")])
   }
   if(any(names(df2)=="Charge.by.Search.Engine.Sequest.HT")){
-    df2<-df2 %>% dplyr::rename("Charge"="Charge.by.Search.Engine.Sequest.HT")
-    
-  }
+    df2<-df2 %>% dplyr::rename("Charge"=names(df2)[stringr::str_detect(names(df2),"Charge")])
+  }else if (any(stringr::str_detect(names(df2),"Charge"))){
+     df2<-df2 %>% dplyr::rename("Charge"=names(df2)[stringr::str_detect(names(df2),"Charge")])
+    }
   
   if(any(names(df2)=="DeltaM.ppm.by.Search.Engine.Sequest.HT")){
     df2<-df2 %>% dplyr::rename("DeltaM"="DeltaM.ppm.by.Search.Engine.Sequest.HT")
     
-  }else if(any(names(df2)=="DeltaM.ppm.")){
-    df2<-df2 %>% dplyr::rename("DeltaM"="DeltaM.ppm.")
+  }else if(any(stringr::str_detect(names(df2),"DeltaM"))){
+    df2<-df2 %>% dplyr::rename("DeltaM"=names(df2)[stringr::str_detect(names(df2),"DeltaM")])
   }
   if(any(names(df2)=="XCorr.by.Search.Engine.Sequest.HT")){
     df2<-df2 %>% dplyr::rename("XCorr"="XCorr.by.Search.Engine.Sequest.HT")
-  }
+  }else if(any(stringr::str_detect(names(df2),"XCorr"))){
+    df2<-df2 %>% dplyr::rename("XCorr"=names(df2)[stringr::str_detect(names(df2),"XCorr")])
+}
   if(any(names(df2)==".Missed.Cleavages")){
     df2<-df2 %>% dplyr::rename("MissedCleavages"=".Missed.Cleavages")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Missed"))){
+    df2<-df2 %>% dplyr::rename("XCorr"=names(df2)[stringr::str_detect(names(df2),"XCorr")])
+}
   if(any(names(df2)=="Annotated.Sequence")){
     df2<-df2 %>% dplyr::rename("Annotated_Sequence"="Annotated.Sequence") %>% 
       dplyr::mutate(Annotated_Sequence=toupper(Annotated_Sequence))
-  }
+  }else if(any(stringr::str_detect(names(df2),"Annotated"))){
+    df2<-df2 %>% dplyr::rename("Annotated_Sequence"=names(df2)[stringr::str_detect(names(df2),"Annotated")])
+}
   if (any(names(df2)=="Master.Protein.Accessions")){
     df2<-df2 %>% dplyr::rename("Accession"="Master.Protein.Accessions")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Master"))){
+    df2<-df2 %>% dplyr::rename("Accession"=names(df2)[stringr::str_detect(names(df2),"Master")])
+}else if(any(stringr::str_detect(names(df2),"Accession"))){
+    df2<-df2 %>% dplyr::rename("Accession"=names(df2)[stringr::str_detect(names(df2),"Accession")])
+}
   if(any(names(df2)=="Average.Reporter.S.N")){
     df2<-df2 %>% dplyr::rename("Average_Reporter_S/N"="Average.Reporter.S.N")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Reporter"))){
+    df2<-df2 %>% dplyr::rename("Average_Reporter_S/N"=names(df2)[stringr::str_detect(names(df2),"Reporter")])
+}
   if(any(names(df2)=="Isolation.Interference.")){
     df2<-df2 %>% dplyr::rename("Isolation_Interference_[%]"="Isolation.Interference.")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Isolation"))){
+    df2<-df2 %>% dplyr::rename("Isolation_Interference_[%]"=names(df2)[stringr::str_detect(names(df2),"Isolation")])
+}
   if(any(names(df2)=="Ion.Inject.Time.ms.")){
     df2<-df2 %>% dplyr::rename("Ion_Inject_Time_[ms]"="Ion.Inject.Time.ms.")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Inject"))){
+    df2<-df2 %>% dplyr::rename("Ion_Inject_Time_[ms]"=names(df2)[stringr::str_detect(names(df2),"Inject")])
+}
   if(any(stringr::str_detect(names(df2),".PSMs"))){
     df2<-df2 %>% dplyr::rename("Num_PSMs"=".PSMs")
-  }
-  if(any(stringr::str_detect(colnames(df2),"Spectrum.File"))){
-    if(any(stringr::str_detect(df2$Spectrum.File,"NOcarrier"))){
+  }else if (any(stringr::str_detect(names(df2),"PSMs"))){
+    df2<-df2 %>% dplyr::rename("Num_PSMs"=names(df2)[stringr::str_detect(names(df2),"PSM")])
+    }
+  if(any(stringr::str_detect(colnames(df2),"Spectrum.File"))){#if this is a PSM file
+    if(any(stringr::str_detect(df2$Spectrum.File,"NOcarrier"))){#if this experiment has Carrier FAIMS and PhiSDM involved, shorten the file names
       df2<-df2 %>% dplyr::mutate(sample_name=paste0(ifelse(stringr::str_detect(Spectrum.File,"NOcarrier")==TRUE,"nC",ifelse(stringr::str_detect(Spectrum.File,"carrier")==TRUE,"C",NA)),'_',
                                                     ifelse(stringr::str_detect(Spectrum.File,"NO_FAIMS")==TRUE,"nF",ifelse(stringr::str_detect(Spectrum.File,"r_FAIMS")==TRUE,"F",NA)),'_',
                                                     ifelse(stringr::str_detect(Spectrum.File,"S_eFT")==TRUE,"E",ifelse(stringr::str_detect(Spectrum.File,"S_Phi")==TRUE,"S",NA))),
@@ -58,12 +77,11 @@ read_xl<-function(x,solvent) {
       if(any(stringr::str_detect(df2$File.ID,"."))){
         df2<-df2 %>% dplyr::mutate(Fraction=stringr::str_remove(.$File.ID,"[[:upper:]]+[[:digit:]]+."),
                                    sample_id = stringr::str_extract(.$File.ID,"F[[:digit:]]+"))
-      }else{
-        df2<-df2 %>% dplyr::mutate(Fraction=stringr::str_remove(.$File.ID,"[[:upper:]]+[[:digit:]]+."),
-                                   sample_id = stringr::str_extract(.$File.ID,"F[[:digit:]]+"))
+      }else{#if theres no fractionation
+        df2<-df2 %>% dplyr::mutate(sample_id = stringr::str_extract(.$File.ID,"F[[:digit:]]+"))
       }
       
-    }else{
+    }else{#if this experiment does not have Carrier FAIMS and PhiSDM involved shorten he file names
       df2<-df2 %>% dplyr::mutate(
         dataset=ifelse(stringr::str_detect(Spectrum.File,solvent),"vehicle","treated"),
         sample_name = ifelse(stringr::str_detect(Spectrum.File,solvent),solvent,stringr::str_extract(stringr::str_to_lower(Spectrum.File),"[[:lower:]]+_[[:digit:]]+_")),
@@ -83,7 +101,7 @@ read_xl<-function(x,solvent) {
 }
 
 
-read_PD<-function(x,solvent){#rename data
+read_PD<-function(x,solvent){#rename data for peptide groups
 
   df2<-x %>% as.data.frame(.) %>% 
     dplyr::select(names(x)[stringr::str_detect(names(x),'Master')],
@@ -109,49 +127,65 @@ read_PD<-function(x,solvent){#rename data
                   -tidyselect::contains('Grouped'),
                   -tidyselect::starts_with('Found')) 
   if(any(names(df2)=="Percolator.PEP.by.Search.Engine.Sequest.HT")){
-    df2<-df2 %>% dplyr::rename("Percolator_PEP"="Percolator.PEP.by.Search.Engine.Sequest.HT")
-  }else if(any(names(df2)=="Percolator.PEP")){
-    df2<-df2 %>% dplyr::rename("Percolator_PEP"="Percolator.PEP")
+    df2<-df2 %>% dplyr::rename("Percolator_PEP"=names(df2)[stringr::str_detect(names(df2),"PEP")])
+  }else if(any(stringr::str_detect(names(df2),"PEP"))){
+    df2<-df2 %>% dplyr::rename("Percolator_PEP"=names(df2)[stringr::str_detect(names(df2),"PEP")])
   }
   if(any(names(df2)=="Charge.by.Search.Engine.Sequest.HT")){
-    df2<-df2 %>% dplyr::rename("Charge"="Charge.by.Search.Engine.Sequest.HT")
-    
-  }
+    df2<-df2 %>% dplyr::rename("Charge"=names(df2)[stringr::str_detect(names(df2),"Charge")])
+  }else if (any(stringr::str_detect(names(df2),"Charge"))){
+     df2<-df2 %>% dplyr::rename("Charge"=names(df2)[stringr::str_detect(names(df2),"Charge")])
+    }
   
   if(any(names(df2)=="DeltaM.ppm.by.Search.Engine.Sequest.HT")){
     df2<-df2 %>% dplyr::rename("DeltaM"="DeltaM.ppm.by.Search.Engine.Sequest.HT")
     
-  }else if(any(names(df2)=="DeltaM.ppm.")){
-    df2<-df2 %>% dplyr::rename("DeltaM"="DeltaM.ppm.")
+  }else if(any(stringr::str_detect(names(df2),"DeltaM"))){
+    df2<-df2 %>% dplyr::rename("DeltaM"=names(df2)[stringr::str_detect(names(df2),"DeltaM")])
   }
   if(any(names(df2)=="XCorr.by.Search.Engine.Sequest.HT")){
     df2<-df2 %>% dplyr::rename("XCorr"="XCorr.by.Search.Engine.Sequest.HT")
-  }
+  }else if(any(stringr::str_detect(names(df2),"XCorr"))){
+    df2<-df2 %>% dplyr::rename("XCorr"=names(df2)[stringr::str_detect(names(df2),"XCorr")])
+}
   if(any(names(df2)==".Missed.Cleavages")){
     df2<-df2 %>% dplyr::rename("MissedCleavages"=".Missed.Cleavages")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Missed"))){
+    df2<-df2 %>% dplyr::rename("XCorr"=names(df2)[stringr::str_detect(names(df2),"XCorr")])
+}
   if(any(names(df2)=="Annotated.Sequence")){
-    df2<-df2 %>% dplyr::rename("Annotated_Sequence"="Annotated.Sequence")%>% 
+    df2<-df2 %>% dplyr::rename("Annotated_Sequence"="Annotated.Sequence") %>% 
       dplyr::mutate(Annotated_Sequence=toupper(Annotated_Sequence))
-  }
+  }else if(any(stringr::str_detect(names(df2),"Annotated"))){
+    df2<-df2 %>% dplyr::rename("Annotated_Sequence"=names(df2)[stringr::str_detect(names(df2),"Annotated")])
+}
   if (any(names(df2)=="Master.Protein.Accessions")){
     df2<-df2 %>% dplyr::rename("Accession"="Master.Protein.Accessions")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Master"))){
+    df2<-df2 %>% dplyr::rename("Accession"=names(df2)[stringr::str_detect(names(df2),"Master")])
+}else if(any(stringr::str_detect(names(df2),"Accession"))){
+    df2<-df2 %>% dplyr::rename("Accession"=names(df2)[stringr::str_detect(names(df2),"Accession")])
+}
   if(any(names(df2)=="Average.Reporter.S.N")){
     df2<-df2 %>% dplyr::rename("Average_Reporter_S/N"="Average.Reporter.S.N")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Reporter"))){
+    df2<-df2 %>% dplyr::rename("Average_Reporter_S/N"=names(df2)[stringr::str_detect(names(df2),"Reporter")])
+}
   if(any(names(df2)=="Isolation.Interference.")){
     df2<-df2 %>% dplyr::rename("Isolation_Interference_[%]"="Isolation.Interference.")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Isolation"))){
+    df2<-df2 %>% dplyr::rename("Isolation_Interference_[%]"=names(df2)[stringr::str_detect(names(df2),"Isolation")])
+}
   if(any(names(df2)=="Ion.Inject.Time.ms.")){
     df2<-df2 %>% dplyr::rename("Ion_Inject_Time_[ms]"="Ion.Inject.Time.ms.")
-  }
-  if(any(names(df2)=="DeltaM.ppm.by.Search.Engine.Sequest")){
-    df2<-df2 %>% dplyr::rename("DeltaM"="DeltaM.ppm.by.Search.Engine.Sequest.HT")
-    
-  }else if(any(names(df2)=="DeltaM.ppm.")){
-    df2<-df2 %>% dplyr::rename("DeltaM"="DeltaM.ppm.")
-  }
+  }else if(any(stringr::str_detect(names(df2),"Inject"))){
+    df2<-df2 %>% dplyr::rename("Ion_Inject_Time_[ms]"=names(df2)[stringr::str_detect(names(df2),"Inject")])
+}
+  if(any(stringr::str_detect(names(df2),".PSMs"))){
+    df2<-df2 %>% dplyr::rename("Num_PSMs"=".PSMs")
+  }else if (any(stringr::str_detect(names(df2),"PSMs"))){
+    df2<-df2 %>% dplyr::rename("Num_PSMs"=names(df2)[stringr::str_detect(names(df2),"PSM")])
+    }
   df2<-df2 %>% 
     tidylog::pivot_longer(cols=colnames(df2)[stringr::str_detect(colnames(df2),"[:digit:][:digit:][:digit:][N|C]|126|131")],
                           names_to = "id",
